3bfa30a490c1dc2c58baf4b133b1934578f87e6d
Emi28000 <cavalino28@gmail.com>
2024-02-12 12:39:36 +0100

    Mes exos

---
+++ fonctions.js
@@ -0,0 +1,19 @@
+//if (presenceDePainauPainAuNoix === true && mamonnaie >= prixPainAuxNoix){
+  // console.log("j'achete du pain au noix")
+  //} else {
+    //console.log("j'acheteune baguette")
+  //}
+
+    //if (8===true == 1>=24)
+    //{
+      //console.log('23') 
+    //} else {
+     // console.log("5,6,7")
+   // }
+
+   let motUtilisateur= prompt("Entrez un mot:")
+   console.log(motUtilisateur)
+   
+   
+   
+
---
+++ script.js
@@ -0,0 +1,89 @@
+//let monAge=35
+//console.log(monAge)
+
+
+// Multiplication de number//
+
+
+//let nombreUtilisateurs=200
+// nombreUtilisateurs -= 25
+
+
+
+//let messageBienvenuePerso="Binevenue,"
+//let nomUtilisateur="David42"
+//messageBienvenuePerso +="David42".
+//let messageBienvenuePerso=messageBienvenue+nomUtilisateur
+//console.log(messageBienvenuePerso+nomUtilisateur)
+//let monPersonnage ={
+//  nom: "Wayne",
+// prenom: "Bruce",
+//  age: 35,
+//  couleurPreferee: "noir",
+//  hobbies: "sortir la nuit",//
+//}
+//monPersonnage.vehicule ="Batmobile"
+//console.log(monPersonnage.couleurPreferee)
+
+
+//Tableau//
+
+
+//const mesFilms=["Titanic","Rambo","Zorro"]
+//mesFilms.push("Mayata")
+//mesFilms.pop()
+//console.log(mesFilms.length)
+
+
+//if('presenceDePainAuxNoix')
+//{
+//console.log("j'achete du pain au noix")
+//}
+//else{
+//   console.log("j'achete une baguette")
+//}
+
+
+//const charlene = (left,right) =>{
+//  return (left+right)/2
+//}
+//console.log(charlene(6,8))
+
+//if (PainAuxNois=== true & maMonnaie>=prixPainAuxNoix) {
+//  console.log("j'achete du pain au noix")
+
+// }else{
+
+//  console.log("j'achete une baguette")
+//}
+
+//for (let compteur = 0; compteur < 10; compteur + 1) {
+// console.log(compteur)
+//}
+
+//let i = 0
+
+//while()
+
+//for (let i=0; i<16; i=i+1){
+  //console.log(i)
+//}
+
+//let i=0
+//while(i<3){
+  //console.log(i)
+  //i++
+//}
+
+for ( let pain= 2; pain>5; pain++) {
+  console.log("Buzz l'eclaire !")
+}
+
+
+
+
+
+
+
+
+
